40baec462be261bc90e0c7a654456ace284679da
takeharak <65995391+takeharak@users.noreply.github.com>
2022-09-03 13:22:48 +0000

    feat(dev): tailwindcss

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -9,14 +9,22 @@ function Counter() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img
+            src="/vite.svg"
+            className="logo inline h-24 p-6"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="logo react inline h-24 p-6"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React + Recoil</h1>
-      <div className="card">
+      <div className="p-8">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
